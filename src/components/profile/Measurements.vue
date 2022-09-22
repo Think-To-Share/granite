dfb6079428ec5f27a73affbb9702b2c636ebaf02
Abir-Das <abird9797@gmail.com>
2022-09-22 20:03:15 +0530

    s m l xl added

--- src/components/profile/Measurements.vue
+++ src/components/profile/Measurements.vue
@@ -1,10 +1,26 @@
 <template>
     <div class="card text-start measurements-card mb-5">
-      <div class="card-body">
+      <div class="card-body button-card">
         <h4 class="card-title granite-card-header mb-5">Do you know your Project Sizes?</h4>
         <div class="round-container">
             <button class="granite-round-btn-filled">YES</button>
             <button class="granite-round-btn-empty">NO</button>
+        </div>
+      </div>
+      <div class="card-body button-card">
+        <h4 class="card-title granite-card-header mb-5">Do you have a Project Plan?</h4>
+        <div class="round-container">
+            <button class="granite-round-btn-filled">YES</button>
+            <button class="granite-round-btn-empty">NO</button>
+        </div>
+      </div>
+      <div class="card-body button-card">
+        <h4 class="card-title granite-card-header mb-5">How would you describe the Size of your Project?</h4>
+        <div class="round-container">
+            <button class="granite-square-btn-empty">S</button>
+            <button class="granite-square-btn-empty">M</button>
+            <button class="granite-square-btn-empty">L</button>
+            <button class="granite-square-btn-empty">XL</button>
         </div>
       </div>
     </div>
@@ -23,6 +39,15 @@ export default {
         border-radius : 0%;
         border : none;
         margin : 10;
+
+        .button-card{
+            border-bottom : 2px solid rgba(250, 250, 250, 0.85);
+            transition: all 0.5s ease 0s;
+
+            &:hover{
+                background-color : rgba(246, 246, 246, 0.85);
+            }
+        }
         .granite-card-header{
             font-family: var(--primary-font);
             color:#3C7C8E;
@@ -59,6 +84,22 @@ export default {
                 padding : 2%;
                 margin : 2%;
                 border-radius : 60%;
+                transition: all 0.5s ease 0s;
+                
+                &:hover{
+                    background-color: #3C7C8E;
+                    border : 6px solid #244a55;
+                    color : #fff;
+                }
+            }
+
+            .granite-square-btn-empty{
+                background-color: #fff;
+                border : 6px solid #ddd;
+                color : #888;
+                font-family:var(--primary-font);
+                padding : 3%;
+                margin : 2%;
                 transition: all 0.5s ease 0s;
                 
                 &:hover{
