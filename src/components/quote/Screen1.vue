95c046a4ab9f70ff80bb592ef5027b76663c1622
Rahul Dey <rahul900day@gmail.com>
2023-01-19 18:15:43 +0530

    updated screen 1

--- src/components/quote/Screen1.vue
+++ src/components/quote/Screen1.vue
@@ -6,8 +6,19 @@
                     <h2 class=" font-medium text-2xl text-gray-500 text-center">Quartz</h2>
                 </div>
 
+                <div class="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(150px,_1fr))] mb-4">
+                    <a v-for="product in quartzProducts" :key="product.id" class="relative hover:scale-110 transition-transform" href="#" @click.prevent="selectProduct(product)">
+                        <img class="w-full" :src="product.attributes.image" />
+                        <h3 class="absolute w-full bottom-0 left-0 px-2 bg-[#69b6d5] text-center font-semibold text-md text-white">{{ product.attributes.name }}</h3>
+                    </a>  
+                </div>
+
+                <div class="w-full bg-gray-200 px-3 py-4 mb-5">
+                    <h2 class=" font-medium text-2xl text-gray-500 text-center">Granite</h2>
+                </div>
+
                 <div class="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(150px,_1fr))]">
-                    <a v-for="product in products" :key="product.id" class="relative hover:scale-110 transition-transform" href="#" @click.prevent="selectProduct(product)">
+                    <a v-for="product in graniteProducts" :key="product.id" class="relative hover:scale-110 transition-transform" href="#" @click.prevent="selectProduct(product)">
                         <img class="w-full" :src="product.attributes.image" />
                         <h3 class="absolute w-full bottom-0 left-0 px-2 bg-[#69b6d5] text-center font-semibold text-md text-white">{{ product.attributes.name }}</h3>
                     </a>  
@@ -49,7 +60,20 @@ export default {
             });
         }
     },
-    components: { Card }
+    components: { Card },
+    computed: {
+        quartzProducts() {
+            return this.products.filter(p => {
+                return p.attributes.category === 'Quartz';
+            })
+        },
+
+        graniteProducts() {
+            return this.products.filter(p => {
+                return p.attributes.category === 'Granite';
+            })
+        }
+    }
 }
 </script>
 
